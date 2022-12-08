3096a49ee7e4919cd9c092d440346689c9fc5a07
antmonnier <monniera@gmail.com>
2022-12-08 11:12:47 +0100

    Export spec_helpers

--- lib/hubrise_money.rb
+++ lib/hubrise_money.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require "hubrise_money/accessors"
 require "hubrise_money/money"
+require "hubrise_money/spec_helpers"
 require "hubrise_money/validator"
 require "hubrise_money/version"
